52833dba0692fed124b5a91c926d3206e47dd2c3
jeongiin <jeongiin1999@gmail.com>
2020-11-12 15:18:48 +0900

    add activity

---
+++ MyPickPlates/app/src/main/java/com/example/myapplication/view/RecommedFoodActivity.kt
@@ -0,0 +1,12 @@
+package com.example.myapplication.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.myapplication.R
+
+class RecommedFoodActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recommed_food)
+    }
+}
---
+++ MyPickPlates/app/src/main/java/com/example/myapplication/view/ViewFoodActivity.kt
@@ -0,0 +1,12 @@
+package com.example.myapplication.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.myapplication.R
+
+class ViewFoodActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_food)
+    }
+}
